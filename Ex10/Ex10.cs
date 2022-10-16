1a5a08986a4c13d3fb3cdf7203255a2cf0b11c4b
kazmi <costellationoblivion@gmail.com>
2022-10-16 23:59:13 +0300

    Добавьте файлы проекта.

--- Ex10/Ex10.cs
+++ Ex10/Ex10.cs
@@ -4,14 +4,17 @@ class Ex10
 {
     static void Main()
     {
-        string?[] temp = Console.ReadLine().Split(' ');
-        Tuple<string, string> tuple1 = new(temp[0] + " " + temp[1], temp[2]);
+        string[] temp = Console.ReadLine().Split(' ');
+        (string Name, string Address) = (temp[0] + " " + temp[1], temp[2]);
+        Tuple<string, string> tuple1 = new(Name, Address);
 
         temp = Console.ReadLine().Split(' ');
-        Tuple<string, int> tuple2 = new(temp[0], Convert.ToInt32(temp[1]));
+        (Name, int LitersOfBeer) = (temp[0], Convert.ToInt32(temp[1]));
+        Tuple<string, int> tuple2 = new(Name, LitersOfBeer);
 
         temp = Console.ReadLine().Split(' ');
-        Tuple<int, float> tuple3 = new(Convert.ToInt32(temp[0]), Convert.ToSingle(temp[1]));
+        (int IntValue, float FloatValue) = (Convert.ToInt32(temp[0]), Convert.ToSingle(temp[1]));
+        Tuple<int, float> tuple3 = new(IntValue, FloatValue);
 
 
         Console.WriteLine("\n" + tuple1);
