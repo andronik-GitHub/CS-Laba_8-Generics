4f664119300d6e85051d76ef9b02fd5c46ac3598
kazmi <costellationoblivion@gmail.com>
2022-10-16 23:59:13 +0300

    Добавьте файлы проекта.

--- Ex10/Ex10.cs
+++ Ex10/Ex10.cs
@@ -4,6 +4,20 @@ class Ex10
 {
     static void Main()
     {
+        string?[] temp = Console.ReadLine().Split(' ');
+        Tuple<string, string> tuple1 = new(temp[0] + " " + temp[1], temp[2]);
+
+        temp = Console.ReadLine().Split(' ');
+        Tuple<string, int> tuple2 = new(temp[0], Convert.ToInt32(temp[1]));
+
+        temp = Console.ReadLine().Split(' ');
+        Tuple<int, float> tuple3 = new(Convert.ToInt32(temp[0]), Convert.ToSingle(temp[1]));
+
+
+        Console.WriteLine("\n" + tuple1);
+        Console.WriteLine(tuple2);
+        Console.WriteLine(tuple3);
+
 
 
         Console.ReadKey();
---
+++ Ex10/Turple.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+class Tuple<T1, T2>
+{
+    public T1 item1;
+    public T2 item2;
+
+    public Tuple(T1 item1, T2 item2)
+    {
+        this.item1 = item1;
+        this.item2 = item2;
+    }
+
+    public override string ToString()
+    {
+        StringBuilder SB = new();
+
+        SB.Append($"{item1} -> {item2}");
+
+        return SB.ToString();
+    }
+}
